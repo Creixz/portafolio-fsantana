0c238538ea5572cc30778b48c5d32c595157b455
creixz <s.freddy131@gmail.com>
2024-02-27 10:24:42 -0500

    portfolio-v1.3

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Personal from './components/personal/Personal'
+import Skills from './components/skills/Skills'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <div className="container">
         <Personal />
+        <Skills />
       </div>
     </>
   )
--- src/components/personal/Personal.jsx
+++ src/components/personal/Personal.jsx
@@ -4,6 +4,8 @@ import { FaReact } from "react-icons/fa6";
 import { FaHtml5 } from "react-icons/fa6";
 import { FaCss3Alt } from "react-icons/fa6";
 import { RiJavascriptFill } from "react-icons/ri";
+import { FaJava } from "react-icons/fa6";
+import { SiSpringboot } from "react-icons/si";
 
 const Personal = () => {
   return (
@@ -14,25 +16,33 @@ const Personal = () => {
       </div>
       <div className="personal__img">
         <div>
-          <div className="tech__icon">
-            <FaReact style={{color: '#04d8f9'}}/>
+          <div className='vertical__icons'>
+            <div className="tech__icon">
+              <SiSpringboot style={{ color: '#6db33f' }} />
+            </div>
+            <div className="tech__icon">
+              <FaReact style={{ color: '#04d8f9' }} />
+            </div>
           </div>
           <img className='fsantana__img' src="../../../fsantana.jpg" alt="" />
         </div>
 
         <div>
           <div className="tech__icon">
-            <FaHtml5 style={{color: 'orangered'}}/>
+            <FaHtml5 style={{ color: 'orangered' }} />
           </div>
           <div className="tech__icon">
-            <FaCss3Alt style={{color: '#264de4'}}/>
+            <FaCss3Alt style={{ color: '#264de4' }} />
           </div>
           <div className="tech__icon">
-           <RiJavascriptFill style={{color: '#f7df1e'}}/>
+            <RiJavascriptFill style={{ color: '#f7df1e' }} />
+          </div>
+          <div className="tech__icon">
+            <FaJava style={{ color: 'red' }} />
           </div>
         </div>
       </div>
-      
+
 
     </section>
   )
